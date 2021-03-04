5e7ebbb9e68d31c0dbb3263a7e2ac223858ddb14
Lee <leemcf4@hotmail.co.uk>
2021-03-04 14:01:30 +0000

    updates

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { motion } from "framer-motion"
 
 export const query = graphql`
